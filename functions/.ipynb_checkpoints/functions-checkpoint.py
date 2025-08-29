976a7b60c66f15d896fd93d7cb2d95193b9a90a8
Mikhail Smilovic <mikhail.smilovic@gmail.com>
2025-08-29 15:09:28 +0200

    Backup

--- functions/.ipynb_checkpoints/functions-checkpoint.py
+++ functions/.ipynb_checkpoints/functions-checkpoint.py
@@ -66,7 +66,9 @@ def read_observations_excel(observations_folder, skiprows=1):
 
             sheet['Date'] = pd.to_datetime(sheet['Date'], format='mixed', dayfirst=True).dt.date
             Dates_observed = sheet['Date'].tolist()
+            sheet['Observation'] = pd.to_numeric(sheet['Observation'], errors='ignore')
             Flows_observed = np.array(sheet['Observation'].tolist())
+            
 
             # negative values transformed to NaN
             Flows_observed = np.where(Flows_observed >= 0, Flows_observed, np.nan)
